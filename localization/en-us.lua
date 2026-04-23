1a7b0e753207a753bbb111ff372835d663084638
cosmeggo <cosmeggo@gmail.com>
2026-04-23 23:32:46 +0100

    minor spelling mistake

---
+++ localization/en-us.lua
@@ -0,0 +1,49 @@
+return {
+    descriptions = {
+        Back = {
+            b_hatchl_holy = {
+                name = "Holy Deck",
+                text = {
+                    "{C:green}Sephirot{} cards may appear",
+                    "in the shop, start with {C:green}Malkuth{}",
+                },
+            },
+        },
+        Blind = {
+            bl_hatchl_axe = {
+                name = "The Axe",
+                text = {
+                    "Destroy first played card",
+                    "after every played hand",
+                },
+            },
+            bl_hatchl_sack = {
+                name = "The Sack",
+                text = {
+                    "Downgrade scored",
+                    "cards by rank",
+                },
+            },
+        },
+        Enhanced = {
+            m_hatchl_sulfur = {
+                name = "Sulfur",
+                text = {
+                    "Permanently gain {C:mult}+1{} Mult when scored",
+                },
+            },
+            m_hatchl_mercury = {
+                name = "Mercury",
+                text = {
+                    "Enhanced card gives between {C:blue}+0{} to {C:blue}+75{} Chips when scored",
+                },
+            },
+            m_hatchl_salt = {
+                name = "Salt",
+                text = {
+                    "Enhanced card has a {C:green}#1# in #2#{} chance to retrigger twice when scored",
+                },
+            },
+        },
+    }
+}
